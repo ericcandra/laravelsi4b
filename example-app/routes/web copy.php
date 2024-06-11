4815c44a407448dfa10ad182aee15da2b0031935
ericcandra <160827840+ericcandra@users.noreply.github.com>
2024-06-11 17:02:04 +0700

    11 06 2024

---
+++ example-app/routes/web copy.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\ProdiController;
+use App\Models\Prodi;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('about', function () {
+    return "halaman about";
+});
+
+Route::get('profil',function () {
+    return view('profil');
+});
+
+// route dengan parameter
+Route::get('welcome/{salam}', function($salam) {
+    // return 'selamat '. $salam;
+    return view('salam')->with('viewsalam', $salam);
+});
+
+// Route tanpa parameter listdata
+// terdapat arrray list
+Route::get('listdata', function() {
+    $list = ["sistem informasi","informatika","manajemen"];
+    $listmhs = [
+        ["npm" => 001, "nama" => "eric"],
+        ["npm" => 002, "nama" => "nabila"],
+    ];
+    return view('listprodi')->with('viewlist', $list)->with('viewmhs', $listmhs);
+});
+
+Route::resource('fakultas', FakultasController::class);
+Route::resource('prodi', ProdiController::class);
+Route::resource('mahasiswa', MahasiswaController::class);
+Route::get('dashboard',[DashboardController::class,'index']);
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -4,39 +4,27 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\ProdiController;
-use App\Models\Prodi;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', function () {
-    return "halaman about";
-});
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('profil',function () {
-    return view('profil');
-});
-
-// route dengan parameter
-Route::get('welcome/{salam}', function($salam) {
-    // return 'selamat '. $salam;
-    return view('salam')->with('viewsalam', $salam);
-});
-
-// Route tanpa parameter listdata
-// terdapat arrray list
-Route::get('listdata', function() {
-    $list = ["sistem informasi","informatika","manajemen"];
-    $listmhs = [
-        ["npm" => 001, "nama" => "eric"],
-        ["npm" => 002, "nama" => "nabila"],
-    ];
-    return view('listprodi')->with('viewlist', $list)->with('viewmhs', $listmhs);
-});
 
 Route::resource('fakultas', FakultasController::class);
 Route::resource('prodi', ProdiController::class);
 Route::resource('mahasiswa', MahasiswaController::class);
-Route::get('dashboard',[DashboardController::class,'index']);
+Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard')->middleware(['auth', 'verified']);
+
+Route::middleware('auth')->group(function () {
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
